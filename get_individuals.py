516d80586fca1280a0ad4a420a5ed2bc89c18ed6
Jazpy <jaz.medtre@gmail.com>
2017-03-24 22:24:19 -0600

    Made individual extraction script's comments clearer

--- get_individuals.py
+++ get_individuals.py
@@ -7,12 +7,12 @@ files = glob.glob('./PopPhased/*.bed')
 # Output file: individuals
 out = open('./individuals', 'w')
 
-# Iterate over all (unique) files
+# Iterate over all files with an unique ID
 i = 0
 while(i < len(files)):
-        # Write the individual's id to out
+        # Write the individual's ID to out
         out.write(files[i][50 : 57] + '\n')
-
+        # Skip over this individual's other chromosome
         i = i + 2
 
 out.close()
