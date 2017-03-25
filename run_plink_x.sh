081e4de6a9f848db095bece4893c0d8a31f983df
Jazpy <jaz.medtre@gmail.com>
2017-03-25 01:23:05 -0600

    Standard qsub script for plink_x.py

---
+++ run_plink_x.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#$ -b y
+#$ -w e
+#$ -e ./logs/err
+#$ -o ./logs/std
+#$ -N extract_AFR_X  # job's name
+
+# run the job in the current working directory
+#$ -cwd
+
+# declares the shell that interprets the job script
+#$ -S /bin/bash
+
+# all environment variables in the qsub commands environment are to be exported to the batch job
+#$ -V
+
+# reserve all requested resources
+#$ -R y
+
+module load python35/3.5.2
+module load plink2/1.90beta3
+
+python ./plink_x.py
